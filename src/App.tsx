20a8d4093bd88a31f2b05025151dace994f5b67e
Filipe Borges <filipebkc2209@gmail.com>
2019-12-31 14:36:30 -0300

    InfoSection desktop layout implemented

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,25 @@ import InfoSection from './components/InfoSection';
 
 const App: React.FC = () => {
   return (
-    <div>
+    <div className="app-container">
       <InfoSection
         title={'My Title is Here'}
         image={logo}
+        index={0}
+      >
+        is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      </InfoSection>
+      <InfoSection
+        title={'My Title is Here'}
+        image={logo}
+        index={1}
+      >
+        is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      </InfoSection>
+      <InfoSection
+        title={'My Title is Here'}
+        image={logo}
+        index={2}
       >
         is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
       </InfoSection>
--- src/components/InfoSection/InfoSection.tsx
+++ src/components/InfoSection/InfoSection.tsx
@@ -4,11 +4,25 @@ import './InfoSection.css';
 interface InfoSectionProps {
   image: string;
   title: string;
+  index?: number;
+}
+
+function buildInlineStyle(props: InfoSectionProps) {
+  const style: React.CSSProperties = {};
+
+  if (typeof props.index === 'number') {
+    style.flexDirection = props.index % 2 ? 'row' : 'row-reverse';
+  }
+
+  return style;
 }
 
 const InfoSection: React.FC<InfoSectionProps> = (props) => {
   return (
-    <div className="container">
+    <div
+      className="container"
+      style={buildInlineStyle(props)}
+    >
       <div>
         <img src={props.image} alt="My pic"/>
       </div>
